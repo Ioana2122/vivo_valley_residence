6fa026a626de7c0894e5f8f8efd411ad64c47437
Mariana Frentsos <marianafrentsos@yahoo.com>
2021-05-14 09:31:03 +0300

    restructuring

---
+++ views/index.js
@@ -0,0 +1,13 @@
+
+document.querySelector("#one-room-app").addEventListener("click", ()=> {
+    window.location.href = '../pages/apartmentOne.html'
+})
+
+document.querySelector("#two-room-app").addEventListener("click", ()=> {
+    window.location.href = '../pages/apartmentTwo.html'
+})
+
+document.querySelector("#three-room-app").addEventListener("click", ()=> {
+    window.location.href = '../pages/apartmentThree.html'
+})
+
