9ff5b8bfaffa3ed93f35c6764bd65b15b7017802
Mariana Frentsos <marianafrentsos@yahoo.com>
2021-05-15 16:35:12 +0300

    feature/Task-4-css and display changes

--- views/details.js
+++ views/details.js
@@ -1,60 +1,56 @@
     const apartments = [
         {
-        title: "Model 1",
-         imageUrl: "../assets/2rooms/ap3.jpg"
+            apartmentNumber: "3",
+            imageUrl: "../assets/2rooms/ap3.jpg"
         },
         {
-        title: "Model 2",
+            apartmentNumber: "4",
         imageUrl: "../assets/2rooms/ap4.jpg"
         },
         {
-            title: "Model 3",
+            apartmentNumber: "5",
             imageUrl: "../assets/2rooms/ap5.jpg"
         },
         {
-            title: "Model 4",
+            apartmentNumber: "6",
             imageUrl: "../assets/2rooms/ap6.jpg"
         },
         {
-            title: "Model 5",
+            apartmentNumber: "10",
             imageUrl: "../assets/2rooms/ap10.jpg"
         },
         {
-            title: "Model 6",
+            apartmentNumber: "11",
             imageUrl: "../assets/2rooms/ap11.jpg"
         },
         {
-            title: "Model 7",
+            apartmentNumber: "15",
             imageUrl: "../assets/2rooms/ap15.jpg"
         },
         {
-            title: "Model 8",
+            apartmentNumber: "16",
             imageUrl: "../assets/2rooms/ap16.jpg"
         },
         {
-            title: "Model 9",
+            apartmentNumber: "20",
             imageUrl: "../assets/2rooms/ap20.jpg"
         },
         {
-            title: "Model 10",
+            apartmentNumber: "21",
             imageUrl: "../assets/2rooms/ap21.jpg"
         }
     ];
 
-    apartments.forEach(apartment => {
-        let apartmentModel  = document.querySelector('.avivo-ap-model')
-        apartmentModel.innerHTML +=`
-            <div class="col">
-                <div class="card h-100 border-0">
-                    <div class="card-body">
-                        <h5 class="card-title fs-2 avivo-ap-model--title">${apartment.title}</h5>
-                    </div>
-                    <picture  class="img-thumbnail avivo-ap-picture">
-                        <source media="(min-width:650px)" srcset="${apartment.imageUrl}">
-                        <source media="(min-width:465px)" srcset="${apartment.imageUrl}">
-                        <img src="${apartment.imageUrl}" alt="Model 1" class="shadow" >
-                    </picture>
-                </div>
-            </div> 
-        `
-    });
+    let apartmentModel  = document.querySelector('.avivo-ap-model');
+    displayModels(apartments,apartmentModel);
+
+    let modalDetails = document.querySelectorAll('.avivo-ap-model--item');
+    displayModalDetails(modalDetails)
+
+    document.querySelector('.go-up-btn-arrow').addEventListener('click', () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    })
